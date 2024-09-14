b26dae71fc7feb73e42f7b0fd9121f9ace15c477
Fareed95 <fareedsayed95@gmail.com>
2024-09-14 11:58:11 +0530

    setup is ready

---
+++ server/shopweb/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
+from .views import index 
+
+urlpatterns = [
+    path('', index, name='index'),
+] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
